2a72846572766d0e02430927c263e2a2b136cf49
Ilya <illyaetoilysha@gmail.com>
2024-10-04 09:04:33 +0000

    Sass build fixed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/saas/main.sass',
+                'resources/sass/main.sass',
                 'resources/js/app.js',
             ],
             refresh: true,
