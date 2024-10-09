50f31c582a0797f8bad5f2cb81ff668f796811e4
Ilya <illyaetoilysha@gmail.com>
2024-10-09 12:05:13 +0000

    Added custom command all-in-one for application deployment & Implemented Sentry library for app monitoring in production and more

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # Installation
 
-- composer install
-- npm install
 - php artisan key:generate
 - php artisan storage:link
 - php artisan migrate
-- sudo chmod 777 -R storage
 
 # Deploy
---
+++ app/Console/Commands/InstallCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the application';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $this->info('Installing the application...');
+
+        $this->call('key:generate');
+        $this->call('storage:link');
+        $this->call('migrate');
+
+        $this->info('Application installed successfully. You can now start using it! 🚀');
+
+        return self::SUCCESS;
+    }
+}
