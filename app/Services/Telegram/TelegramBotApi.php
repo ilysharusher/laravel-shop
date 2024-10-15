2e9b32d73bbd78708dea3121dfc9faad9aecb8d6
Ilya <illyaetoilysha@gmail.com>
2024-10-15 12:51:02 +0000

    Removed unnecessary PHP Docs

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -10,14 +10,6 @@ class TelegramBotApi
 {
     public const BASE_URL = 'https://api.telegram.org/bot';
 
-    /**
-     * Sends a message to a specified Telegram chat.
-     *
-     * @param string $token The bot token to authenticate the request.
-     * @param int $chatId The ID of the chat where the message will be sent.
-     * @param string $text The text of the message to be sent.
-     * @return bool Returns true if the message was sent successfully, false otherwise.
-     */
     public static function sendMessage(string $token, int $chatId, string $text): bool
     {
         try {
