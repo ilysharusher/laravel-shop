a8157137b3e5436a224325b3e4088ead8485ce98
Ilya <illyaetoilysha@gmail.com>
2024-10-18 14:11:29 +0000

    Implemented functional for adding index to slug for making it unique if it needs

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -11,10 +11,19 @@ trait HasSlug
         return 'title';
     }
 
+    private static function addIndexToSlug(string $slug): string
+    {
+        $count = self::query()->where('slug', 'like', "{$slug}%")->count();
+
+        return $count ? "-{$count}" : '';
+    }
+
     protected static function bootHasSlug(): void
     {
         static::creating(static function (Model $instance) {
-            $instance->slug ??= str($instance->{self::slugFrom()})->append(time())->slug();
+            $baseSlug = str($instance->{static::slugFrom()})->slug();
+
+            $instance->slug ??= $baseSlug . self::addIndexToSlug($baseSlug);
         });
     }
 }
