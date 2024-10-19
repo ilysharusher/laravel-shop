e811c4b5d5ef96b38db06211b59b69a2dc72e71d
Ilya <illyaetoilysha@gmail.com>
2024-10-19 13:58:39 +0000

    Custom command for simple fresh migrates

---
+++ app/Console/Commands/FreshMigrationsCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class FreshMigrationsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:fresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fresh migrations and seeders';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        if (app()->isLocal()) {
+            Storage::deleteDirectory('images');
+
+            $this->call('migrate:fresh', ['--seed' => true]);
+        } else {
+            $this->error('This command can only be run in the local environment.');
+        }
+    }
+}
