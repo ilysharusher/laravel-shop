e4d3f79d5c9bf85d1808a4902cef6c2a27d03cb5
Ilya <illyaetoilysha@gmail.com>
2024-10-22 10:06:12 +0000

    Some frontend changes

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TestServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    App\Providers\TestServiceProvider::class,
+    App\Providers\ViewServiceProvider::class,
 ];
