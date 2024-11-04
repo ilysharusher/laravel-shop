7c12201c93d6ab20cb94090f0f1b019705a36866
Ilya <illyaetoilysha@gmail.com>
2024-11-04 13:48:18 +0000

    Added PHP Docs to custom faker command

--- app/Faker/FakerThumbnailProvider.php
+++ app/Faker/FakerThumbnailProvider.php
@@ -7,6 +7,14 @@ use Illuminate\Support\Facades\Storage;
 
 class FakerThumbnailProvider extends Base
 {
+    /**
+     * Generates a random thumbnail file from a source directory to a target directory.
+     *
+     * @param string $from The source directory relative to 'tests/Fixtures/'.
+     * @param string $to The target directory relative to 'storage/app/public/'.
+     * @param bool $fullPath Whether to return the full path of the file.
+     * @return string The path to the generated thumbnail file.
+     */
     public function randomThumbnail(string $from, string $to, bool $fullPath = false): string
     {
         if (!Storage::exists($to)) {
